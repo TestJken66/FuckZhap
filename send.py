b5b8e4d5c506f747afe31289ef783d4cca4b4139
sanbo <sanbo.xyz@gmail.com>
2024-02-21 20:07:50 +0800

    update sth. at 2024-02-21 20:07:50

--- send.py
+++ send.py
@@ -5,17 +5,17 @@ from email.header import Header
 # 第三方 SMTP 服务
 mail_host="smtp.163.com"  #设置服务器
 mail_user="chinapolicy110_365@163.com"    #用户名
-mail_pass=""   #口令  
+mail_pass="AUPZIUNSKYCZMUVA"   #口令  
  
  
 sender = 'chinapolicy110_365@163.com'
-receivers = ['chinapolicy110_365@163.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 
+receivers = ['chinapolicy110_365@163.com','ecochainliesstop@gmail.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 
  
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] =  Header("测试", 'utf-8')
+message = MIMEText('【警惕网络诈骗,人人有责】!', 'plain', 'utf-8')
+message['From'] = Header("chinapolicy110_365@163.com", 'utf-8')
+message['To'] =  Header("ecochainliesstop@gmail.com", 'utf-8')
  
-subject = 'Python SMTP 邮件测试'
+subject = '【警惕网络诈骗,人人有责】'
 message['Subject'] = Header(subject, 'utf-8')
  
  
