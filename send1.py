d4812a087340a8121012bdc175b336245f508c35
sanbo <sanbo.xyz@gmail.com>
2024-02-27 16:54:58 +0800

    update sth. at 2024-02-27 16:54:58

--- send1.py
+++ send1.py
@@ -4,12 +4,16 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 from email.header import Header
+
+
 import os
 # 从环境变量获取口令
 mail_pass = os.getenv('EMAIL_PASS', '默认值')
+
 # 第三方 SMTP 服务
 mail_host = "smtp.163.com"  # 设置服务器
 mail_user = "chinapolicy110_365@163.com"    # 用户名
+# mail_pass = ""   # 口令
 
 sender = 'chinapolicy110_365@163.com'
 receivers = ['ecochainliesstop@gmail.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
@@ -17,17 +21,9 @@ receivers = ['ecochainliesstop@gmail.com']  # 接收邮件，可设置为你的Q
 # 创建一个带附件的实例
 message = MIMEMultipart()
 message['From'] = Header("chinapolicy110_365@163.com", 'utf-8')
-message['To'] = Header("ecochainliesstop@gmail.com", 'utf-8')
+message['To'] = "ecochainliesstop@gmail.com"  # 直接使用字符串赋值
 subject = '【警惕网络诈骗,人人有责】'
 message['Subject'] = Header(subject, 'utf-8')
-
-# 标记邮件为高优先级
-message['X-Priority'] = '1'  # 最高优先级
-message['Priority'] = 'urgent'
-message['Importance'] = 'high'
-
-# 请求阅读回执
-message['Disposition-Notification-To'] = sender
 
 # 邮件正文内容
 message.attach(MIMEText('警惕网络诈骗,人人有责,不作恶，无作恶！！', 'plain', 'utf-8'))
