1078fcc8fdebba0d4fa60021f80cef9f7e8d0caf
sanbo <sanbo.xyz@gmail.com>
2024-02-27 15:49:06 +0800

    update sth. at 2024-02-27 15:49:06

--- send.py
+++ send.py
@@ -9,7 +9,7 @@ mail_pass=""   #口令
  
  
 sender = 'chinapolicy110_365@163.com'
-receivers = ['chinapolicy110_365@163.com','ecochainliesstop@gmail.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 
+receivers = 'ecochainliesstop@gmail.com'  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱 
  
 message = MIMEText('【警惕网络诈骗,人人有责】!', 'plain', 'utf-8')
 message['From'] = Header("chinapolicy110_365@163.com", 'utf-8')
@@ -19,12 +19,12 @@ subject = '【警惕网络诈骗,人人有责】'
 message['Subject'] = Header(subject, 'utf-8')
  
  
-for i in range(100):
-    try:
-        smtpObj = smtplib.SMTP() 
-        smtpObj.connect(mail_host,25)    # 25 为 SMTP 端口号
-        smtpObj.login(mail_user,mail_pass)
-        smtpObj.sendmail(sender, receivers, message.as_string())
-        print ("邮件发送成功")
-    except smtplib.SMTPException:
-        print ("Error: 无法发送邮件")
+# for i in range(100):
+try:
+    smtpObj = smtplib.SMTP() 
+    smtpObj.connect(mail_host,25)    # 25 为 SMTP 端口号
+    smtpObj.login(mail_user,mail_pass)
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print ("邮件发送成功")
+except smtplib.SMTPException:
+    print ("Error: 无法发送邮件")
